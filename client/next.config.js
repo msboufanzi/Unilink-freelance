df10d9052b7aceb7fc6963ad125eac009c0d9978
msboufanzi <mohameds.boufanzi@gmail.com>
2025-02-09 00:50:55 -0500

    test

--- client/next.config.js
+++ client/next.config.js
@@ -2,17 +2,7 @@
 const nextConfig = {
   // reactStrictMode: process.env.NODE_ENV !== "production",
   images: {
-    remotePatterns: [
-      {
-        protocol: process.env.NODE_ENV === "production" ? "https" : "http",
-        hostname:
-          process.env.NODE_ENV === "production"
-            ? "https://unilink-freelance.onrender.com"
-            : "https://unilink-freelance.onrender.com",
-        port: process.env.NODE_ENV === "production" ? "" : "5001",
-        pathname: "**",
-      },
-    ],
+    domains: ["unilink-freelance.onrender.com"],
   },
 };
 
