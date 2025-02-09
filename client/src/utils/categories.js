373200eab367bcaf773f591e44a22f0eed145f07
msboufanzi <mohameds.boufanzi@gmail.com>
2025-02-09 06:28:13 -0500

    change categories

--- client/src/utils/categories.js
+++ client/src/utils/categories.js
@@ -1,10 +1,10 @@
 export const categories = [
-  { name: "Graphic Design", logo: "/service-1.svg" },
+  // { name: "Graphic Design", logo: "/service-1.svg" },
   { name: "Tutoring", logo: "/service-2.svg" },
   { name: "Resume Writing", logo: "/service-3.svg" },
   { name: "Project Help", logo: "/service-4.svg" },
   { name: "Music & PPT Assistance", logo: "/service-5.svg" },
-  // { name: "Programming & Tech", logo: "/service-6.svg" },
+  { name: "Graphic Design", logo: "/service-1.svg" },
   // { name: "Business", logo: "/service-7.svg" },
   // { name: "Lifestyle", logo: "/service-8.svg" },
   // { name: "Data", logo: "/service-9.svg" },
